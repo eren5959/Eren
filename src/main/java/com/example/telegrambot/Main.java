de8038e80a55949a693e952965197f39987e6a64
eren5959 <thefoundingtitane@gmail.com>
2025-08-16 13:54:48 +0300

    Main.java

---
+++ src/main/java/com/example/telegrambot/Main.java
@@ -0,0 +1,16 @@
+package com.example.telegrambot;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new MyBot());
+            System.out.println("Bot started successfully!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
